215c5e93b7f386d88aab1808ed54ea64b70a53ab
R.Jones <RayshawnJones1991@gmail.com>
2024-05-09 17:16:54 -0400

    modern-javascript-syntax-lab

---
+++ app.js
@@ -0,0 +1,45 @@
+// Use `.map()` to iterate over the following array:
+
+// const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+
+// Your code here
+
+// const nums2 = nums.map(num => num * 2);
+
+// console.log(nums);
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// Your code here
+// const [topping1, topping2] = pizzaToppings;
+// console.log(topping1);
+// console.log(topping2);
+// Equivalent
+// console.log(pizzaToppings[0]);
+// console.log(pizzaToppings[1]);
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+//   };
+//   // Your code here
+//   // Object destructuring:
+// const { make, model } = car;
+
+// console.log(make); // 'Alex'
+// console.log(model); // 'Software Engineer'
+
+// Equivalent in traditional dot notation:
+// console.log(car.make); // 'Audi'
+// console.log(car.model); // 'q5
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// Your code here
+const controversialPizzaToppings = [...pizzaToppings];
+console.log(controversialPizzaToppings); 
